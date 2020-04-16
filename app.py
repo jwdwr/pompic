90ddf34c58bd8aa77fe7d62fa04373a197e51962
James Dewar <james@arcs.in>
2020-04-16 15:11:17 -0400

    upload dream to s3

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
+import os
+from uuid import uuid1
+
+import boto3
+s3_client = boto3.client('s3')
+
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
-def dream():
+def index():
   return "You are dreaming."
+
+@app.route('/save', methods=['POST'])
+def save():
+  entry = request.json
+
+  # generate upload url + params
+  bucket = os.environ.get('DREAMS_BUCKET')
+  key = str(uuid1())
+  conditions = [{"acl": "private"}]
+
+  return s3_client.generate_presigned_post(bucket, key, Conditions=conditions)
