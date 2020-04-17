3320e2509614726bb08de345958f7a9f7c58cb92
James Dewar <james@arcs.in>
2020-04-16 22:48:04 -0400

    authorize, save to dynamodb

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 from uuid import uuid1
-
 import boto3
-s3_client = boto3.client('s3')
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -14,10 +12,29 @@ def index():
 @app.route('/save', methods=['POST'])
 def save():
   entry = request.json
+  dream_id = entry.get('dream_id') or str(uuid1())
+
+  event = request.environ['serverless.event']
+  user_id = event['requestContext']['identity']['cognitoIdentityId']
+
+  # save to database
+  dream = {
+    "user_id": user_id,
+    "dream_id": dream_id,
+    "metadata": entry['metadata']
+  }
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('dreams')
+  table.put_item(Item=dream)
 
   # generate upload url + params
+  s3_client = boto3.client('s3')
   bucket = os.environ.get('DREAMS_BUCKET')
-  key = str(uuid1())
+  key = user_id + '/' + dream_id
   conditions = [{"acl": "private"}]
 
   return s3_client.generate_presigned_post(bucket, key, Conditions=conditions)
+
+@app.route('/list', methods=['GET'])
+def list():
+  return {"dreams": dynamo_client.scan(TableName='dreams')['Items']}
